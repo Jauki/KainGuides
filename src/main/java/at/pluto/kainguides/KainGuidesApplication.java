bde2472c9f3af3da7f00d53f67ce4494ce8b0fb1
blamac <max.blauensteiner@sigadev.com>
2022-12-16 16:15:17 +0100

    init : initial commit

---
+++ src/main/java/at/pluto/kainguides/KainGuidesApplication.java
@@ -0,0 +1,13 @@
+package at.pluto.kainguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KainGuidesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KainGuidesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/pluto/kainguides/KainGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package at.pluto.kainguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KainGuidesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
