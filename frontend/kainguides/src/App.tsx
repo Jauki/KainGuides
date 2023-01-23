4bbc4d6f0d63d6143de3ed881edfee3e899b7c29
halelc18 <halelc18@htl-kaindorf.at>
2023-01-23 15:59:50 +0100

    merge:feat/guideList

--- frontend/kainguides/src/App.tsx
+++ frontend/kainguides/src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { Route, Routes, Router } from 'react-router-dom';
-import UserProfile from './pages/user/UserProfilePage';
-
+import React from "react";
+import { Route, Routes, Router } from "react-router-dom";
+import UserProfile from "./pages/user/UserProfilePage";
 
 function App() {
   return (
-    <div className='w-screen h-screen'>  
-        <Routes>
-          <Route path='/user' element={<UserProfile/>}></Route>
-        </Routes>
+    <div className="w-screen h-screen">
+      <Routes>
+        <Route path="/user" element={<UserProfile />}></Route>
+      </Routes>
     </div>
-    );
+  );
 }
 
 export default App;
